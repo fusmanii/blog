923c67d304018863bfa10d8ff15a7c89d5b8ea38
Faisal Usmani <faisal.usmani@mail.utoronto.ca>
2016-08-09 22:35:05 -0400

    Added: Post tamplate with post/permalink route

--- blog.py
+++ blog.py
@@ -5,8 +5,12 @@ import bottle
 from models.users import users as USERS
 from models.posts import posts as POSTS
 from models.sessions import sessions as SESSIONS
+from bottle import SimpleTemplate
+
+
 
 app = bottle.Bottle()
+SimpleTemplate.defaults["get_url"] = app.get_url
 
 @app.route('/')
 def blogIndex():
@@ -20,8 +24,10 @@ def blogIndex():
 
     posts = POSTS.getPosts(10)
 
-    return bottle.template('index', dict(
-            posts=posts, username=username, get_url=app.get_url))
+    return bottle.template(
+            'index',
+            dict(posts=posts, username=username)
+    )
 
 @app.route('/tag/<tag>')
 def postByTag(tag='notfound'):
@@ -36,16 +42,48 @@ def postByTag(tag='notfound'):
 
     posts = POSTS.getPostsByTag(tag, 10)
 
-    return bottle.template('index', dict(posts=posts, username=username, get_url=app.get_url))
+    return bottle.template(
+            'index',
+            dict(posts=posts, username=username)
+    )
+
+@app.get('/post/<permalink>')
+def showPost(permalink):
+    '''
+    Specific post by permalink.
+    '''
+
+    cookie = bottle.request.get_cookie('session')
+
+    username = SESSIONS.getUsername(cookie)
+    post = POSTS.getPostByPermalink(html.escape(permalink))
+
+    comment = {
+        'name': '',
+        'body': '',
+        'email': ''
+    }
+
+    return bottle.template(
+            'post',
+            dict(
+                post=post,
+                username=username,
+                errors='',
+                comment=comment
+            )
+    )
 
 @app.route('/<filename:path>', name='static')
 def server_static(filename):
     '''
-    Routing for css and js files.
+    Routing for static files.
     '''
-    print(filename)
-    return bottle.static_file(filename, root='/Users/faisalusmani/Documents/blog/blog/views/')
+
+    return bottle.static_file(
+            filename,
+            root='/Users/faisalusmani/Documents/blog/blog/views/'
+    )
 
 if __name__ == '__main__':
-    #bottle.debug(True)
     app.run(host='localhost', port=8000, debug=True)
